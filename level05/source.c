b1cd153d12cce6d2ac27047773041401d5dad50d
Stefan <47391841+ostef@users.noreply.github.com>
2025-11-03 17:28:59 +0100

    Level04 WIP

---
+++ level05/source.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char** argv, char** envp) {
+    int i = 0;
+    char buff[100];
+
+    fgets(buff, 100, stdin);
+    int len = strlen(buff);
+    while (i < len) {
+        if (buff[i] >= 'A' && buff[i] <= 'Z') {
+            buff[i] += 'a' - 'A';
+        }
+        i += 1;
+    }
+
+    printf(buff);
+    exit(0);
+}
