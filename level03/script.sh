b5f4390f43d010d94e75e25cb8d574961fa49ad0
Stefan <oumansour.stefan@gmail.com>
2025-12-11 13:53:50 +0100

    Added level03/script.sh

---
+++ level03/script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo 322424844 | ~/level03
+echo 322424843 | ~/level03
+echo 322424842 | ~/level03
+echo 322424841 | ~/level03
+echo 322424840 | ~/level03
+echo 322424839 | ~/level03
+echo 322424838 | ~/level03
+echo 322424837 | ~/level03
+echo 322424836 | ~/level03
+echo 322424829 | ~/level03
+echo 322424828 | ~/level03
+echo 322424827 | ~/level03
+echo 322424826 | ~/level03
+echo 322424825 | ~/level03
+echo 322424824 | ~/level03
